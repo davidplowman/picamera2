a3726aaefdeb3f2c654b1023c5c56fdac2501216
David Plowman <david.plowman@raspberrypi.com>
2025-05-13 14:23:56 +0100

    Fix up PFC tests

--- tests/pfc.py
+++ tests/pfc.py
@@ -1,40 +1,37 @@
 import time
+
 from picamera2 import Picamera2
 
-last_sync_id = 0
-last_md = {}
 
 def print_info(sync_id, md):
     exp = md['ExposureTime']
     ag = md['AnalogueGain']
     dg = md['DigitalGain']
-    print(sync_id, "exp", exp, "ag", round(ag, 2), "dg", round(dg, 2), end="")
+    print(sync_id, "exp", exp, "ag", round(ag, 2), "dg", round(dg, 2))
+
 
 def pfc_callback(request):
-    global last_sync_id, last_md
     md = request.get_metadata()
     sync_id = request.sync_id
-    if sync_id != last_sync_id:
-        print_info(sync_id, md)
-        print(" \tpreviously: ", end="")
-        print_info(last_sync_id, last_md)
-        print("")
-    last_md = md
-    last_sync_id = sync_id
-    
+    print_info(sync_id, md)
+
+
 picam2 = Picamera2()
 picam2.pre_callback = pfc_callback
 
-picam2.configure()
+controls = {'FrameRate': 30}
+config = picam2.create_preview_configuration(controls=controls)
+picam2.configure(config)
 picam2.start(show_preview=True)
 
-time.sleep(2)
+time.sleep(1)
 
-picam2.set_controls({'ExposureTime': 10000})
-time.sleep(2)
+picam2.set_controls({'ExposureTime': 1000, 'AnalogueGain': 3.0})
+time.sleep(1)
 
-picam2.set_controls({'ExposureTime': 0, 'AnalogueGain': 1})
-time.sleep(2)
+picam2.set_controls({'ExposureTime': 20000, 'AnalogueGain': 1.3})
+time.sleep(1)
 
 sync_id = picam2.set_controls({'ExposureTime': 0, 'AnalogueGain': 0})
+time.sleep(0.5)
 picam2.capture_request(sync_id=sync_id)
--- tests/pfc2.py
+++ tests/pfc2.py
@@ -1,29 +1,31 @@
-from numbers import Number
 import sys
-import time
+from numbers import Number
+
 from picamera2 import Picamera2
 
 picam2 = Picamera2()
 
 config = picam2.create_preview_configuration(raw={'size': (1332, 990), 'format': 'SBGGR10_CSI2P'},
                                              buffer_count=3)
+
 picam2.configure(config)
-picam2.set_controls({'FrameRate': 120})
+picam2.set_controls({'FrameRate': 60})
 picam2.start(show_preview=False)
 
 controls_list = [
-    {'ExposureTime': 1000, 'AnalogueGain': 1, 'Saturation': 0.5},
+    {'ExposureTime': 1000, 'AnalogueGain': 1.2, 'Saturation': 0.5},
     {'ExposureTime': 2000, 'AnalogueGain': 2, 'Saturation': 0.6},
     {'ExposureTime': 3000, 'AnalogueGain': 3, 'Saturation': 0.7},
-    {'ExposureTime': 1000, 'AnalogueGain': 1.1, 'Saturation': 0.8},
+    {'ExposureTime': 1000, 'AnalogueGain': 1.3, 'Saturation': 0.8},
     {'ExposureTime': 2000, 'AnalogueGain': 2.1, 'Saturation': 0.9},
     {'ExposureTime': 3000, 'AnalogueGain': 3.1, 'Saturation': 1.0},
-    {'ExposureTime': 1000, 'AnalogueGain': 1.2, 'Saturation': 1.1},
+    {'ExposureTime': 1000, 'AnalogueGain': 1.4, 'Saturation': 1.1},
     {'ExposureTime': 2000, 'AnalogueGain': 2.2, 'Saturation': 1.2},
     {'ExposureTime': 3000, 'AnalogueGain': 3.2, 'Saturation': 1.3}
-    ]
+]
 
 images = []
+
 
 def callback(ctrl_id, request):
     def do_round(val):
@@ -36,9 +38,10 @@ def callback(ctrl_id, request):
     thresh = 0.01
     for key in expected.keys():
         if isinstance(got[key], Number) and \
-        (got[key] > (1 + thresh) * expected[key] or got[key] < (1 - thresh) * expected[key]):
+           (got[key] > (1 + thresh) * expected[key] or got[key] < (1 - thresh) * expected[key]):
             print("ERROR - mismatch in setting", key)
             sys.exit(-1)
     images.append(request.make_array('main'))
+
 
 print(picam2.capture_sequence(controls_list, callback))
